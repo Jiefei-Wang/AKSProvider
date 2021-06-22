c1158d1fd6be6aa9010c0343680f2367d97bdd3f
Jiefei Wang <szwjf08@gmail.com>
2021-06-22 14:53:56 +0800

    Fix some check issues

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,8 @@
 #' @import DockerParallel
 #' @import yaml
 #' @import jsonlite
+#' @import methods
+#' @import utils
 #' @importFrom readr read_file write_file
 #' @importFrom stringr str_order
 NULL
